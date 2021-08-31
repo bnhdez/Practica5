7ff90f771253b7055db3763339a2067f1fb48d61
BORIS NICHOLAS HERNANDEZ NUÑEZ <2021010562@cvirtualuees.edu.sv>
2021-08-31 15:02:16 -0600

    cambios

---
+++ Ejercicio_2/Ejercicio_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ejer_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte K, SW;
+            string pass;
+            SW = 0; K = 0;
+            do
+            {
+                Console.Write("DIGITE CLAVE: "); pass = Console.ReadLine();
+                if (pass == "contra123")
+                {
+                    SW = 1;
+                }
+                else
+                {
+                    K++;
+                }
+            } while (((K < 3) & (SW == 0)));
+            if (SW == 1)
+            {
+                Console.WriteLine("BIENVENIDO ");
+            }
+            else
+            {
+                Console.WriteLine("OPORTUNIDADES TERMINADAS");
+            }
+        }
+    }
+}
